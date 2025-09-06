d8e2f42a1ad6a0229492cddfc48255217647f7c4
Kenneth Carter <kccarter32@gmail.com>
2025-09-05 19:49:52 -0600

    no message

--- SubLightQueryProvider.cs
+++ SubLightQueryProvider.cs
@@ -1,16 +1,17 @@
+using SubLight.Query;
 using System.Linq.Expressions;
 
 namespace SubLight;
 
 public sealed class SubLightQueryProvider
-    : ISubLighQueryProvider
+    : ISubLightQueryProvider
 {
     public IQueryable CreateQuery(Expression expression)
     {
         throw new NotImplementedException();
     }
 
-    public IQueryable<TEntity> CreateQuery<TEntity>(Expression expression)
+    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
     {
         throw new NotImplementedException();
     }
@@ -25,7 +26,7 @@ public sealed class SubLightQueryProvider
         throw new NotImplementedException();
     }
 
-    public string Translate(IQueryTranslator translator)
+    public TResult Translate<TResult>(Expression expression, IQueryTranslator<TResult> translator) where TResult : IQueryResult
     {
         throw new NotImplementedException();
     }
